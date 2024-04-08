122f248b1c518fb8d543d1b93b833dc6b3855493
Adithya Krishna S <quark365microbot@outlook.com>
2024-04-08 10:06:03 +0530

    Create test_cipher_flex.py

---
+++ tests/test_cipher_flex.py
@@ -0,0 +1,8 @@
+# test_cipher_flex.py
+from CipherFlex import CipherFlex
+
+def test_encrypt_decrypt():
+    encryption = CipherFlex()
+    key, encrypted_data = encryption.encrypt("password123")
+    _, decrypted_data = encryption.decrypt(encrypted_data)
+    assert decrypted_data == "password123"
